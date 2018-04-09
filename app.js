512df04c988edc34d127b8ddf5bafed77d80beca
gabrielSpassos <gabrielsantos45725@gmail.com>
2018-04-09 14:14:11 -0300

    :sparkles: add request to wire-mock server with node app

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var request = require("request")
 
 var app = express();
 
@@ -7,10 +8,15 @@ app.get('/', function (req, res) {
     console.log('Olar');
 });
 
-app.get('/api/v1/pessoas', function (req, res) {
-    console.log('funcina');
+app.get('/api/v1/people', function (req, res) {
+    request('http://localhost:9090/api/pessoas/2647', function (error, response, body) {
+        console.log(' ------- ')
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+      });
 });
 
-app.listen(3000, function () { // Inicia uma requisição do Express
-    console.log('Escutando em http://localhost:3000');
+app.listen(4500, function () { // Inicia uma requisição do Express
+    console.log('Escutando em http://localhost:4500');
 });
