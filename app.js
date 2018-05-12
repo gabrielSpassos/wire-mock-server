9638d8ed96c6fcb2b34af63d5a75ca24e591268b
gabrielSpassos <gabrielsantos45725@gmail.com>
2018-05-12 18:26:34 -0300

    :sparkles: enviando body do wire-mock para a tela

--- app.js
+++ app.js
@@ -4,15 +4,32 @@ var request = require("request")
 
 var app = express();
 
-app.get('/', function (req, res) {
-    console.log('Olar');
-});
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+const router = express.Router();
+router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+app.use('/', router);
 
 app.get('/api/v1/people/2647', function (req, res) {
     request('http://localhost:9090/api/pessoas/2647', function (error, response, body) {
         if(!error && response.statusCode == 200){
             console.log(' ------- ')
             console.log('body:', body);
+            res.json(body);
+        }
+    });
+});
+
+app.get('/api/pessoas-detalhes/2647', function (req, res) {
+    request('http://localhost:9090/api/pessoas-detalhes/2647', function (error, response, body) {
+        var title = 'Teste do WireMock'
+        if(!error && response.statusCode == 200){
+            res.render('people', {
+                title: title,
+                body: body, 
+            });
         }
     });
 });
